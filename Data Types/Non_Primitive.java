de765e2bf537b3136934ac14fbfb7c41858d459d
dyannadle <dyannadle05@gmail.com>
2025-11-15 23:45:24 +0530

    Auto commit on 11/15/2025 23:45:23

--- Data Types/Non_Primitive.java
+++ Data Types/Non_Primitive.java
@@ -6,12 +6,12 @@ enum Color {
 public class Non_Primitive {
     public static void main(String[] args) {
         String str = "Hello, World!";
-        int arr ={1,2,3,4,5};
+        int[] arr ={1,2,3,4,5};
         Color clr = Color.RED;
         System.out.println(clr);
         System.err.println(str);
         for (int member: arr){
-            System.out.println(member +",");
+        System.out.print(member +",");
         }
     }
 }
