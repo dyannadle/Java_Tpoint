3fcf8fb1542c7fb10c1d55dc070b848487e4ecc4
dyannadle <dyannadle05@gmail.com>
2025-11-16 00:57:04 +0530

    Auto commit on 11/16/2025 00:57:04

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+public class ArithmeticOperator {
+public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+
+    // Addition
+    int sum = a + b;
+    System.out.println("Addition: " + sum);
+
+    // Subtraction
+    int difference = a - b;
+    System.out.println("Subtraction: " + difference);
+
+    // Multiplication
+    int product = a * b;
+    System.out.println("Multiplication: " + product);
+
+    // Division
+    int quotient = a / b;
+    System.out.println("Division: " + quotient);
+
+    // Modulus
+    int remainder = a % b;
+    System.out.println("Modulus: " + remainder);     
+}
+}
