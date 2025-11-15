bdeedbd3329c18130509482c83bbbf26f449f9d8
dyannadle <dyannadle05@gmail.com>
2025-11-16 01:20:43 +0530

    Auto commit on 11/16/2025 01:20:43

---
+++ Operators/BitwiseAndOperater.java
@@ -0,0 +1,8 @@
+public class BitwiseAndOperater {
+public stsic void main(String args[]){
+    int a=11;//1011
+    int b=5;// 0101
+    //a&b=1011 & 0101=0001=1
+    System.out.println("The avlue of a&b is: "+(a&b)); //AND operator
+}    
+}
