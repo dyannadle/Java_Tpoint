4f2df6b9301b07b19da85a9045132410ac4be00f
dyannadle <dyannadle05@gmail.com>
2025-11-16 00:17:07 +0530

    Auto commit on 11/16/2025 00:17:07

--- Unicode/ConvertUTF8ToUnicode.java
+++ Unicode/ConvertUTF8ToUnicode.java
@@ -1,12 +1,12 @@
+package Unicode;
+import java.nio.charset.StandardCharsets;
+
 public class ConvertUTF8ToUnicode {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         String str1 = "Unicode System\u00A9";   // UTF-8 String
-        byte [] charset = str1.getBytes("UTF-8");// Converting UTF-8 String to byte array
-        String newstr = new String(charset, "UTF-8");// Converting byte array to Unicode String
+        byte[] charset = str1.getBytes(StandardCharsets.UTF_8); // Converting UTF-8 String to byte array
+        String newstr = new String(charset, StandardCharsets.UTF_8); // Converting byte array to Unicode String
         System.out.println("Original UTF-8 String: " + str1);
         System.out.println("Converted Unicode String: " + newstr);
-} 
+    }
 }
-
-
-
