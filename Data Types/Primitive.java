76a324cf77a4c6365a346ce1fb20510d2abf3714
dyannadle <dyannadle05@gmail.com>
2025-11-15 23:40:16 +0530

    Auto commit on 11/15/2025 23:40:16

---
+++ Data Types/Primitive.java
@@ -0,0 +1,23 @@
+
+public class Primitive {
+public static void main(String[] args) {
+    boolean flag = true;
+    char letter = 'A';
+    int number = 1000;
+    byte size = 100;
+    short srt = 78;
+    double value = 67.89;
+    float temp = 56.78f;
+    long val = 123456789L;
+
+
+    System.out.println("boolean value: " + flag);
+    System.out.println("char value: " + letter);
+    System.out.println("int value: " + number); 
+    System.out.println("byte value: " + size);
+    System.out.println("short value: " + srt);
+    System.out.println("double value: " + value);
+    System.out.println("float value: " + temp); 
+    System.out.println("long value: " + val);
+}  
+}
