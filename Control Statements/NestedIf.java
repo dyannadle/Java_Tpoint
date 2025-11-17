8a15042ce94ae841c2ad7109fe2d7ade720abcc1
dyannadle <dyannadle05@gmail.com>
2025-11-17 22:20:55 +0530

    Auto commit on 11/17/2025 22:20:55

---
+++ Control Statements/NestedIf.java
@@ -0,0 +1,32 @@
+// if(condition 1) {    
+// statement 1; //executes when condition 1 is true   
+// if(condition 2) {  
+// statement 2; //executes when condition 2 is true   
+// }  
+// else{  
+// statement 2; //executes when condition 2 is false   
+// }  
+// }  
+
+public class NestedIf {
+    
+public static void main(String args []){
+    String address="india , mumbai, area , andheri east";
+    if address.endsWith("India"));{
+if address.contains("mumbai")){
+    System.out.println("your city is mumbai");
+}else(address.contains("pune")){
+    System.out.println("your city is pune");    
+}else(address.contains("nagpur")){
+    System.out.println("your city is nagpur");  
+}else{
+    System.out.println(address.split(","[0]));
+}
+    }else{
+    System.out.println("you are not in india");
+      
+
+    }
+}
+
+}
