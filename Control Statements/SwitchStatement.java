8d504e7b93d38aa017e73b8a7a646a7e372a1f62
dyannadle <dyannadle05@gmail.com>
2025-11-17 22:45:31 +0530

    Auto commit on 11/17/2025 22:45:31

---
+++ Control Statements/SwitchStatement.java
@@ -0,0 +1,36 @@
+// switch (expression){  
+//     case value1:  
+//      statement1;  
+//      break;  
+//     .  
+//     .  
+//     .  
+//     case valueN:  
+//      statementN;  
+//      break;  
+//     default:  
+//      default statement;  
+// }  
+
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+int num=2;
+Switch num=2;
+switch (num){
+    casse 0:
+        System.out.println("number is zero");
+        break;
+    case 1:
+        System.out.println("number is one");
+        break;
+    case 2:
+        System.out.println("number is two");
+        break;
+    default:
+        System.out.println("number is greater than two");
+    }
+    }
+}
+
+
