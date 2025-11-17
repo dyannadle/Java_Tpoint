de6dbd24a2ec63e2153bc4073b36a436d9689fcb
dyannadle <dyannadle05@gmail.com>
2025-11-17 21:11:04 +0530

    Auto commit on 11/17/2025 21:11:03

---
+++ Java Keywords/JavaKeywords.java
@@ -0,0 +1,17 @@
+
+public class JavaKeywords {
+        public static void main(String[] args)      
+    {      
+        //Using int keyword    
+        int a=10;    
+        int b=20;    
+        //Using if-else keywords    
+        if(a<b){    
+            System.out.println(a+" is less than "+b);    
+        }   
+     else {    
+            System.out.println(a+" is greater than "+b);    
+        }    
+    }      
+}  
+
