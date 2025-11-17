663e9bda607d1acdddaeec93d918a73a23019d29
dyannadle <dyannadle05@gmail.com>
2025-11-17 21:44:58 +0530

    Auto commit on 11/17/2025 21:44:58

--- Control Statements/Simple_If.java
+++ Control Statements/Simple_If.java
@@ -1,10 +1,13 @@
 public class Simple_If {
     public static void main(String[] args) {
-int x = 15;
+int x = 5;
 int y= 10;
 if(x+y>20)
 {
             System.out.println("x+y is geater than 20");
+        }
+        else {
+            System.out.println("x+y is less than or equal to 20");
         }
     }
 }
