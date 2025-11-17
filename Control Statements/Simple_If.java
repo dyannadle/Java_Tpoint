294161553b19140b3ff61c8333e8cc1c615dd2e9
dyannadle <dyannadle05@gmail.com>
2025-11-17 21:39:47 +0530

    Auto commit on 11/17/2025 21:39:46

---
+++ Control Statements/Simple_If.java
@@ -0,0 +1,10 @@
+public class Simple_If {
+    public static void main(String[] args) {
+int x = 15;
+int y= 10;
+if(x+y>20)
+{
+            System.out.println(number + " is an even number.");
+        }
+    }
+}
