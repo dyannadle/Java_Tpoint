1778bfb810d9c7d43e4eeb255830cb0799f055bc
dyannadle <dyannadle05@gmail.com>
2025-11-18 23:09:22 +0530

    Auto commit on 11/18/2025 23:09:22

---
+++ Control Statements/ForLoop.java
@@ -0,0 +1,8 @@
+public ForLoop{
+    public static void main (String[] args) {
+        int sum=0;
+        for(int j=1; j<=10;j++){
+            System.err.println("Sum of first 10 natural number is  " + sum);
+        }
+        }
+    }
