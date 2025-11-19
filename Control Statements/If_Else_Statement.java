282e09ee82bd4ff3f23342f92032dc42300732b7
dyannadle <dyannadle05@gmail.com>
2025-11-19 22:01:45 +0530

    Auto commit on 11/19/2025 22:01:45

---
+++ Control Statements/If_Else_Statement.java
@@ -0,0 +1,23 @@
+// if(condition){  
+// //code if condition is true  
+// }else{  
+// //code if condition is false  
+// }  
+
+
+
+
+
+
+public class If_Else_Statement {
+    public static void main (String args []){
+        int num=13;
+        if(num%2==0){
+            System.out.println(num+" is even number");
+        }else{
+            System.out.println(num+" is odd number");
+            }
+        }
+    }
+
+
