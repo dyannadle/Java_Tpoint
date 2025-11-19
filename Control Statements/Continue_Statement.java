1a64fa96d9ca86d20320a26b2e7a7e57e30dfe46
dyannadle <dyannadle05@gmail.com>
2025-11-19 21:40:15 +0530

    Auto commit on 11/19/2025 21:40:15

---
+++ Control Statements/Continue_Statement.java
@@ -0,0 +1,14 @@
+public class Continue_Statement {
+    public static void main(String args []){
+        for (int i=0;i<=2; i++){
+            
+            for (int j=i;j<=5;j++){
+                if (j==4){
+                    continue;
+
+                }
+                System.out.println(j);
+            }
+        }
+    }
+}
