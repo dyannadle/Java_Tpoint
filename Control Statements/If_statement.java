02d65664a1c4e6773948a8c37cf6df46f17d1976
dyannadle <dyannadle05@gmail.com>
2025-11-19 21:45:24 +0530

    Auto commit on 11/19/2025 21:45:23

---
+++ Control Statements/If_statement.java
@@ -0,0 +1,15 @@
+// if(condition){  
+// //code to be executed  
+// }  
+
+
+
+public class If_statement {
+    public static void main (String args[]){
+        int age=20;
+        if(age >18 ){
+            System.out.println("The person is adult ");
+        }
+    }
+    
+}
