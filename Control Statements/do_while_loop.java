a8cdd59c3214cc7b51c57c53d92424e876371660
dyannadle <dyannadle05@gmail.com>
2025-11-19 20:37:43 +0530

    Auto commit on 11/19/2025 20:37:43

---
+++ Control Statements/do_while_loop.java
@@ -0,0 +1,17 @@
+// do     
+// {    
+// //statements    
+// } while (condition);   
+
+
+
+public class do_while_loop {
+public static void main(String args[]){
+    int i=0;
+    System.out.println("printing list of first 10 even numbers :");
+    do{
+        System.out.println(i);
+        i=i+2;
+    } while (i<=10);
+}    
+}
