5fb9062cbe5cef1a4bcc528681095f60416d5fab
dyannadle <dyannadle05@gmail.com>
2025-11-19 21:28:58 +0530

    Auto commit on 11/19/2025 21:28:58

---
+++ Control Statements/Break_Statement.java
@@ -0,0 +1,10 @@
+public class Break_Statement {
+    public static void main (String args[]){
+        for(int i=0;i<=10;i++){
+               System.out.println(i);
+            if(i==5){
+                break;
+            }
+        }
+    }
+}
