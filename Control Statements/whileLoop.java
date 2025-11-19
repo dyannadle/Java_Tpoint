c7b3caeb43e5ecc545ac99a10c80853f5659b6b4
dyannadle <dyannadle05@gmail.com>
2025-11-19 20:26:19 +0530

    Auto commit on 11/19/2025 20:26:19

---
+++ Control Statements/whileLoop.java
@@ -0,0 +1,19 @@
+// while(condition){    
+// //looping statements    
+// }    
+
+
+
+
+public class whileLoop {
+public static void main (String args[]){
+    int i=0;
+    System.out.println("Printing list of first 10 even numbers :");
+    while(i<=10){
+        System.out.println(i++);
+        i=i+2;
+
+    }
+}
+    
+}
