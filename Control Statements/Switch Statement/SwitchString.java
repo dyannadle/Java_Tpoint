d7605567c22dd028886f6baaa923e68e66c9522e
dyannadle <dyannadle05@gmail.com>
2025-11-22 15:46:34 +0530

    Auto commit on 11/22/2025 15:46:34

---
+++ Control Statements/Switch Statement/SwitchString.java
@@ -0,0 +1,19 @@
+public class SwitchString {
+    public static void main(String[] args) {
+ String levelString="Expert";    
+    int level=0;    
+    //Using String in Switch expression    
+    switch(levelString){      
+    //Using String Literals in Switch Case    
+    case "Beginner": level=1;    
+    break;      
+    case "Intermediate": level=2;    
+    break;      
+    case "Expert": level=3;    
+    break;      
+    default: level=0;    
+    break;    
+    }      
+    System.out.println("Your Level is: "+level);    
+}      
+}  
