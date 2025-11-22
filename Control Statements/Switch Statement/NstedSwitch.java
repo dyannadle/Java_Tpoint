39601c9537b02b0ac74913541c0df315e5787a12
dyannadle <dyannadle05@gmail.com>
2025-11-22 15:55:49 +0530

    Auto commit on 11/22/2025 15:55:48

---
+++ Control Statements/Switch Statement/NstedSwitch.java
@@ -0,0 +1,44 @@
+public class NstedSwitch {
+    public static void main(String[] args) {
+    char branch = 'C';
+    int collegeYear = 4;    
+    switch(collegeYear){
+    case 1: 
+        System.out.println("English, Maths, Science"); 
+        break;
+    case 2:
+        System.out.println("English, Maths, Science, Java, Data Structure"); 
+        break;
+    case 3:
+        switch(branch){
+        case 'C':
+            System.out.println("Operating System, Computer Networks, DBMS, Java");
+            break;
+        case 'E':
+            System.out.println("Microprocessors, Logic Switching Theory, Circuit Analysis, Java");
+            break;
+        case 'M':
+            System.out.println("Thermodynamics, Fluid Mechanics, Heat Transfer, Java");
+            break;
+        }
+        break;
+    case 4:
+
+        switch(branch){
+        case 'C':
+            System.out.println("Data Mining, Machine Learning, AI, Big Data");
+            break;
+        case 'E':
+            System.out.println("Embedded System, Image Processing, Signal Processing, IoT");
+            break;
+        case 'M':
+            System.out.println("Production Technology, Thermal Engineering, Refrigeration and Air Conditioning, IoT");
+            break;
+        }
+        break;
+    default:
+        System.out.println("Invalid College Year");
+        break;
+    }
+}
+}
