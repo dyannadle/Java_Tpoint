85e3bedcc4757c25c0e9ded4767f0d9f79e87662
dyannadle <dyannadle05@gmail.com>
2025-11-23 20:10:04 +0530

    Auto commit on 11/23/2025 20:10:04

---
+++ ForLoop/pyramid.java
@@ -0,0 +1,14 @@
+public class pyramid {
+    public static void main(String[] args) {
+
+        // Outer loop for each row
+        for (int i=1;i<=5;i++)) {
+            // Inner loop for spaces
+            for (  for(int j=1;j<=i;j++) {
+                System.out.print(" *");
+         
+            }
+            System.out.println();
+        }
+    }
+}   
