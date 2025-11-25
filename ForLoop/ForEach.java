0f6b50ed7d0a8b767e888de5c78362998403eae2
dyannadle <dyannadle05@gmail.com>
2025-11-25 21:07:18 +0530

    Auto commit on 11/25/2025 21:07:18

---
+++ ForLoop/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach{
+    public static void main(String[] args) {
+        int arr[]={ 12,23,44,56,78};
+
+        // Using for-each loop to iterate through the array
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+}
