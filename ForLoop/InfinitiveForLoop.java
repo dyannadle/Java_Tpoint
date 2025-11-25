0eb0b13d5ed228c3262810217f5e489ad0edde69
dyannadle <dyannadle05@gmail.com>
2025-11-25 21:21:41 +0530

    Auto commit on 11/25/2025 21:21:41

---
+++ ForLoop/InfinitiveForLoop.java
@@ -0,0 +1,15 @@
+// for(;;){  
+// //code to be executed  
+// }  
+
+
+
+
+public class InfinitiveForLoop {
+    public static void main(String[] args) {
+        // Infinite for loop
+        for (;;) {
+            System.out.println("This loop will run forever.");
+        }
+    }
+}
