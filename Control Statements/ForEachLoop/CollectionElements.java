2c4f437134bbdf8c68b60f0aa3962455d57d7d7f
dyannadle <dyannadle05@gmail.com>
2025-11-26 21:24:28 +0530

    Auto commit on 11/26/2025 21:24:28

---
+++ Control Statements/ForEachLoop/CollectionElements.java
@@ -0,0 +1,15 @@
+public class CollectionElements {
+    public static void main(String[] args) {
+    ArrayList<String>list = new ArrayList<String>();
+        list.add("Apple");
+        list.add("Banana"); 
+        list.add("Mango");
+        list.add("Orange");
+        List<String> fruits = list;
+        
+        // Using for-each loop to traverse the collection elements
+        for (String s:list) {
+            System.out.println(s);
+        }
+    }
+}   
