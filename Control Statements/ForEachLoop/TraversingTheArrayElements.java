43e61668b2042a0d57f77691886cec01f6e91222
dyannadle <dyannadle05@gmail.com>
2025-11-26 21:06:03 +0530

    Auto commit on 11/26/2025 21:06:03

---
+++ Control Statements/ForEachLoop/TraversingTheArrayElements.java
@@ -0,0 +1,10 @@
+public class TraversingTheArrayElements {
+    public static void main(String[] args) {
+        int arr[]={12,13,14,55};
+        
+        // Using for-each loop to traverse the array
+        for (int i:arr) {
+            System.out.println(i);
+        }
+    }
+}
