00bc17ceef646534c5a0c572e6fa01ed9dade289
dyannadle <dyannadle05@gmail.com>
2025-11-27 22:17:41 +0530

    Auto commit on 11/27/2025 22:17:41

---
+++ Control Statements/Do-While Loop/Example.java
@@ -0,0 +1,11 @@
+public class Example {      
+public static void main(String[] args) {    
+    //initialization    
+    int i=1;      
+    //do-while loop  
+    do{      
+        System.out.println(i);      
+    i++;      
+    }while(i<=10);      
+}      
+}      
