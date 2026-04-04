ad6e54787c59da723b1b0da4eb66220bbe354863
dyannadle <dyannadle05@gmail.com>
2026-04-04 11:51:03 +0530

    feat: add ArmyEligibility check program and new method example class

---
+++ Control Statements/if-else Statement/ArmyEligibility.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArmyEligibility {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double minHeight = 5.5;
+        double maxHeight= 6.1;
+        double minWeight = 60.0 ; 
+        double maxWeight = 80.0;
+        
+        System.out.println("--- Army Eligibility Checker ---");
+
+
+        System.out.print("Enter your height (in feet): ");
+        double height = scanner.nextDouble();
+
+        System.out.print("Enter your weight (in kg): ");
+        double weight = scanner.nextDouble();
+
+     
+        if (height >= minHeight) {
+            if (weight >= minWeight && weight <= maxWeight) {
+                System.out.println("\nCongratulations! You are eligible for the army.");
+            } else {
+                System.out.println("\nResult: Not Eligible.");
+                System.out.println("Reason: Your weight must be between " + minWeight + "kg and " + maxWeight + "kg.");
+            }
+        } else {
+            System.out.println("\nResult: Not Eligible.");
+            System.out.println("Reason: Your height must be at least " + minHeight + " cm.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ Q-Spider_327/MethodEx2.java
@@ -0,0 +1,31 @@
+
+public class MethodEx2 {
+    public static void main(String[] args) {
+        add(9, 'a');
+        sub(6, 'b');
+        mul(5, 'c');
+        div(20, 'd');
+        mod(50, 10);
+    }
+
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+
+    }
+
+    public static void sub(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public static void mul(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public static void div(int a, int b) {
+        System.out.println(a / b);
+    }
+
+    public static void mod(int a, int b) {
+        System.out.println(a % b);
+    }
+}
